e8e57455a70e6db407989473fd66d03f6c9e51ea
Jaikant Bhagwan Das <jaikantjk@gmail.com>
2022-09-23 22:24:38 +0530

    Update EyConsumerThread.java

--- EyConsumerThread.java
+++ EyConsumerThread.java
@@ -1,4 +1,4 @@
-package au.com.ing.microservice.notificationproducer.consumer;
+//package au.com.ing.microservice.notificationproducer.consumer;
 
 //Copyright (c) Microsoft Corporation. All rights reserved.
 
@@ -19,9 +19,9 @@ import org.apache.kafka.common.errors.TopicAuthorizationException;
 import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
-import lombok.extern.slf4j.Slf4j;
+//import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
+//@Slf4j
 public class EyConsumerThread implements Runnable {
 
     private static int id;
@@ -31,15 +31,20 @@ public class EyConsumerThread implements Runnable {
     private String secondarySharedAccesskey;
     private String jaasConfigValue;
     private final String jaasConfigProperty;
+    private Consumer<Long, String> consumer;
     // Each consumer needs a unique client ID per thread
 
 
     public EyConsumerThread(final String topic) {
         this.topic = topic;
-        this.primarySharedAccessKey = "EuWuwuWhdM76x3TImruH85r1HZ+lmI+7kfy9d2YqJxU=";
-        this.secondarySharedAccesskey = "dFJPkZtkDuiarZggM2NYXn8tKBgMrJhGWSnJMG2IdLA=";
+        this.primarySharedAccessKey = "u1kCGnfYCGSj2Tzy6bDotcV18S4ZT8UmxfGJanbMtDU=";
+        this.secondarySharedAccesskey = "JWasKBRjic0GsrUM6/UYS1aKy+miXtXvAlPjI0f5+y0=";
+        //this.jaasConfigValue = "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"$ConnectionString\""
+        //        + " password=\"Endpoint=sb://ausupsi00eaeh03.servicebus.windows.net/;SharedAccessKeyName=FuseEventNotificationListener;SharedAccessKey=_SAK_;EntityPath=fuse-event-notification\";";
         this.jaasConfigValue = "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"$ConnectionString\""
-                + " password=\"Endpoint=sb://ausupsi00eaeh03.servicebus.windows.net/;SharedAccessKeyName=FuseEventNotificationListener;SharedAccessKey=_SAK_;EntityPath=fuse-event-notification\";";
+                       + " password=\"Endpoint=sb://jaifirstnamespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=_SAK_;EntityPath=secondeventhub\";";
+        
+        //Endpoint=sb://jaifirstnamespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=u1kCGnfYCGSj2Tzy6bDotcV18S4ZT8UmxfGJanbMtDu=
         this.jaasConfigProperty = "sasl.jaas.config";
         this.isPrimaryKeyInUse = false;
     }
@@ -47,35 +52,39 @@ public class EyConsumerThread implements Runnable {
     public void run() throws RuntimeException {
 
         while (true) {
-            final Consumer<Long, String> consumer = createConsumer();
-            log.info("Polling");
+        	final Consumer<Long, String> consumer = createConsumer();
+            consumer.subscribe(Collections.singletonList(topic));
+            System.out.println("Polling");
             try {
                 while (true) {
                     final ConsumerRecords<Long, String> consumerRecords = consumer.poll(1000);
                     for (ConsumerRecord<Long, String> cr : consumerRecords) {
-                        log.info(String.format("Consumer Record:(%d, %s, %d, %d)%n", cr.key(), cr.value(), cr.partition(),
+                        System.out.println(String.format("Consumer Record:(%d, %s, %d, %d)%n", cr.key(), cr.value(), cr.partition(),
                                 cr.offset()));
                     }
                     consumer.commitAsync();
                 }
             } catch (TopicAuthorizationException e) {
-                log.info("TopicAuthorizationException : " + e);
+            	System.out.println("TopicAuthorizationException : " + e);
             } catch (CommitFailedException e) {
-                log.info("CommitFailedException: " + e);
+            	System.out.println("CommitFailedException: " + e);
 
+            } finally {
+            		consumer.close();
+            	}            		
             }
         }
 
-    }
+    //}
 
     public Consumer<Long, String> createConsumer() throws RuntimeException {
         // Create the consumer using properties.
-        try (final Consumer<Long, String> consumer = new KafkaConsumer<>(createProperties())) {
+          return  new KafkaConsumer<>(createProperties());
             // Subscribe to the topic.
-            consumer.subscribe(Collections.singletonList(topic));
-            return consumer;
+            //consumer.subscribe(Collections.singletonList(topic));
+           // return consumer;
 
-        }
+        //}
     }
 
     private Properties createProperties() throws RuntimeException {
@@ -91,15 +100,14 @@ public class EyConsumerThread implements Runnable {
             properties.load(is);
             this.isPrimaryKeyInUse = !this.isPrimaryKeyInUse;
             String secretKey = this.isPrimaryKeyInUse ? this.primarySharedAccessKey : this.secondarySharedAccesskey;
-            log.info("secret key: " + secretKey);
+            System.out.println("secret key: " + secretKey);
             properties.put(this.jaasConfigProperty, this.jaasConfigValue.replaceAll("_SAK_", secretKey));
             return  properties;
         } catch (IOException e) {
-            log.info("IOException: " + e);
+        	System.out.println("IOException: " + e);
             throw new  RuntimeException();
         }
 
     }
 
 }
-
